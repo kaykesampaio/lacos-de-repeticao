aa85af9d42e03e16e8f7dd06653cc89acda6ad52
Kayke Sampaio Freitas <kaykesampaio196@gmail.com>
2022-06-07 02:07:52 -0300

    .

--- exercicio6.py
+++ exercicio6.py
@@ -1,11 +1,13 @@
 print('👻' * 25)
 
-p = int(input('\nPrimeiro termo 👉 '))
+p = int(input('\nPrimeiro termo 👉 ')) #primeiro
 
-r = int(input('Razão 👉 '))
+r = int(input('Razão 👉 ')) # razão
 
-for c in range(1, 10, 1):
-    print('\n{} '.format(c), end=' 👉 ')
+d = p + (10 - 1) * r # Décimo
+
+for c in range(p, d + r, r):
+    print('{} '.format(c), end='👉 ')
 print(' ACABOU.')
 
 print('👻' * 25)
---
+++ exercicio7.py
@@ -0,0 +1,23 @@
+
+núm = int(inout('Digite um número'))
+
+tot = 0
+
+for c in range(1, núm,1):
+    if núm % c == 0:
+        print('\033[33m', end='')
+        tot = tot + 1
+    else:
+        print('\033[31m', end='')
+
+
+
+
+
+
+
+
+
+
+
+
